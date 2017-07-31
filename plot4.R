8b3c303bc3384836445521c494ae7d2bde87042e
Yuval Yellin <yuvalyellin@gmail.com>
2017-08-01 00:03:08 +0300

    edit file

--- plot4.R
+++ plot4.R
@@ -13,10 +13,14 @@ table <- table[complete.cases(table),]
 table$dateTime <- paste(table$Date, table$Time)
 table$dateTime <- as.POSIXct(dateTime)
 
+par(mfrow=c(2,2), mar=c(4,4,2,1), oma=c(0,0,2,0))
+plot(table$Global_active_power~table$dateTime, type="l",     ylab="Global Active Power (kilowatts)", xlab="")
+plot(table$Voltage~table$dateTime, type="l", ylab="Voltage (volt)", xlab="")
 plot(table$Sub_metering_1~table$dateTime, type="l", ylab="Global Active Power (kilowatts)", xlab="")
 lines(table$Sub_metering_2~table$dateTime,col='Red')
 lines(table$Sub_metering_3~table$dateTime,col='Blue')
-legend("topright", col=c("black", "red", "blue"), lwd=c(1,1,1),  c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+plot(table$Global_reactive_power~table$dateTime, type="l", ylab="Global Rective Power (kilowatts)",xlab="")
 
-dev.copy(png,"plot3.png", width=480, height=480)
+dev.copy(png,"plot4.png", width=480, height=480)
 dev.off()
